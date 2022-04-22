ed9825ef8426438f402b67c43348b1c3ae8fddf3
CristianITB <94795781+CristianITB@users.noreply.github.com>
2022-04-22 09:22:53 +0200

    Update DOMExercici4.js

--- ExercicisDOM/DOMExercici4.js
+++ ExercicisDOM/DOMExercici4.js
@@ -1,12 +1,22 @@
 function obtenirAtributs(){
-    const element = document.getElementById('itb');
+    var element = document.getElementById('itb');
     
+    /*
     const attrNames = element.getAttributeNames();
     console.log(attrNames); // 👉️ ['id', 'data-id', 'class']
 
     const attrValues = element
     .getAttributeNames()
     .map(name => element.getAttribute(name));
-  
+
     console.log(attrValues); // 👉️ ['blue', 'example', 'box']
+    */
+    let attrNames = element.getAttributeNames();
+
+    let attribute_array = [];
+
+    for (i = 0; i < attrNames.length; i++){
+        attribute_array.push(element.getAttribute(attrNames[i]))
+    }
+    console.log(attribute_array);
 }
